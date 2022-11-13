b2283f54b43fab659bed240e6f43c45bfa3ad93b
PRITAM PRAKASH JATHAR <pritamjat98@gmail.com>
2022-11-13 23:01:34 +0530

    Add files via upload

---
+++ chat1.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+import openai
+
+load_dotenv()
+openai.api_key = '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'
+completion = openai.Completion()
+
+start_chat_log = '''Human: Hello, who are you?
+AI: I am doing great. How can I help you today?
+'''
+
+def ask(question, chat_log=None):
+    if chat_log is None:
+        chat_log = start_chat_log
+    prompt = f'{chat_log}Human: {question}\nAI:'
+    response = completion.create(
+        prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
+        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+        max_tokens=150)
+    answer = response.choices[0].text.strip()
+    return answer
+
+print(ask('Who played Forrest Gump in the movie?'))
+print(ask('How long does it take to travel from Los Angeles to Dublin?'))
